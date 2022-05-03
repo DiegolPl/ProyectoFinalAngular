311ae9fa70dff17bb324cef8975c82c2a67b9885
DiegolPl <diegopdl1992@gmail.com>
2022-05-02 22:27:22 -0300

    Fix ventana modal edit componente educaciones

--- src/app/componentes/educaciones/educaciones.component.ts
+++ src/app/componentes/educaciones/educaciones.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-educaciones',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./educaciones.component.css']
 })
 export class EducacionesComponent implements OnInit {
+
+  faTimes = faTimes;
 
   constructor() { }
 
