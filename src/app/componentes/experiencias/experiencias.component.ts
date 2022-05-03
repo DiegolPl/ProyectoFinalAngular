8f7dd2a2a22b21329af41326cc13e4294d32f78a
DiegolPl <diegopdl1992@gmail.com>
2022-05-02 22:30:09 -0300

    Fix ventana modal edit componente experiencias

--- src/app/componentes/experiencias/experiencias.component.ts
+++ src/app/componentes/experiencias/experiencias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-experiencias',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencias.component.css']
 })
 export class ExperienciasComponent implements OnInit {
+
+  faTimes = faTimes;
 
   constructor() { }
 
