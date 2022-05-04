c5c96f85059ab9c285d7835d8cb77e18151c5b90
DiegolPl <diegopdl1992@gmail.com>
2022-05-04 09:33:15 -0300

    fix boton menu y apertura/cierre  de login

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -17,27 +17,24 @@ export class HeaderComponent implements OnInit {
   faUser = faUser;
   faBars = faBars;
 
-  hidden = false;
-  hiddenMenuBtn = false;
-  hiddenCloseBtn = true;
-
   activarMenu(){
-    this.hidden = true;
-    this.hiddenMenuBtn = true;
-    this.hiddenCloseBtn = false;
+    document.getElementById('menu-navbar')?.classList.toggle('active');
+    document.getElementById('menu-btn-close')?.classList.toggle('d-none');
+    document.getElementById('menu-btn')?.classList.toggle('d-none');
   }
 
   desactivarMenu(){
-    this.hidden = false;
-    this.hiddenMenuBtn = false;
-    this.hiddenCloseBtn = true;
+    document.getElementById('menu-navbar')?.classList.toggle('active');
+    document.getElementById('menu-btn-close')?.classList.toggle('d-none');
+    document.getElementById('menu-btn')?.classList.toggle('d-none');
   }
 
   abrirLogin(){
     document.getElementById('modal-login')?.classList.toggle('modal-ventana-active');
     document.getElementById('menu-btn-close')?.classList.add('d-none');
     document.getElementById('menu-navbar')?.classList.remove('active');
-    document.getElementById('menu-btn')?.classList.add('d-block');
+    document.getElementById('menu-btn')?.classList.remove('d-none');
+    document.getElementById('menu-btn')?.classList.add('d-block d-md-none');
   }
 
   constructor() { }
--- src/app/componentes/login-modal/login-modal.component.ts
+++ src/app/componentes/login-modal/login-modal.component.ts
@@ -10,6 +10,16 @@ export class LoginModalComponent implements OnInit {
 
   faTimes = faTimes;
 
+  cerrarLogin(){
+    document.getElementById('modal-login')?.classList.remove('modal-ventana-active');
+    let userInput = document.getElementById('user-input') as HTMLInputElement;
+    let passInput = document.getElementById('pass-input') as HTMLInputElement;
+    userInput.value = '';
+    passInput.value = '';
+    document.getElementById('label-login-user')?.classList.remove('label-top');
+    document.getElementById('label-login-pass')?.classList.remove('label-top');
+  }
+
   constructor() { }
 
   ngOnInit(): void {
